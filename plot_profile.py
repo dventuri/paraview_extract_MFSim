06e1711b76597ea6929970289e25a8ee7cf3f2ad
dventuri <diegoventuri@gmail.com>
2022-03-24 14:16:56 -0300

    removed blank line

--- plot_profile.py
+++ plot_profile.py
@@ -11,4 +11,3 @@ circle = plt.Circle((0.5,0.5), 0.3207, color='white', fill=False, linewidth=1)
 fig, ax = plt.subplots(figsize=(6,6))
 ax.pcolor(yy, zz, plot_var, edgecolors='k', linewidths=1)
 ax.add_patch(circle)
-
